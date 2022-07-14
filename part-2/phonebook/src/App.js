7618d709a5b82d7763b64c7444cb70cb803b9a07
happi89 <farhaanp99@gmail.com>
2022-07-13 23:46:57 -0400

    finshed 2c

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,11 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
 	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then((response) => setPersons(response.data));
+	}, []);
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
