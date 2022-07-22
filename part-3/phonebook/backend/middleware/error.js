18a93ac8d8e54e9d8147ea11c78abd9774262f91
happi89 <farhaanp99@gmail.com>
2022-07-21 20:23:54 -0400

    finished part 3

---
+++ part-3/phonebook/backend/middleware/error.js
@@ -0,0 +1,15 @@
+// const { modelName } = require('../models/person.model');
+
+// const errorHandler = (error, req, res, next) => {
+// 	console.log(error.message);
+
+// 	if (error.name === 'CastError') {
+// 		return res.status(400).json({ error: 'malformatted id' });
+// 	}
+
+// 	next(error);
+// };
+
+// app.use(errorHandler);
+
+// module.exports = errorHandler;
